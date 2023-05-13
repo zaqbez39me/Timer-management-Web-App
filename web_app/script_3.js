d43a6d09c92bb23a895c69e948df83474bb287ba
VadigullinElmir1 <89780734+VadigullinElmir1@users.noreply.github.com>
2023-05-13 20:27:50 +0300

    Update script_3.js

--- web_app/script_3.js
+++ web_app/script_3.js
@@ -1,94 +1,120 @@
-forms = document.getElementsByClassName("timer")
-datePicker = document.forms[0].querySelector(".input-date")
-inputTitle = ''
-inputDate = ''
-inputValue = Date
-let timerActive;
-isPaused = false
-const today = new Date().toISOString().split("T")[0]
-datePicker.setAttribute("min", today)
+function timeMe() {
+    containers = document.getElementsByClassName("timer-container")
+    forms = containers[containers.length - 1].getElementsByClassName("timer")
+    datePicker = forms[0].querySelectorAll(".input-date")[1]
+    const today = new Date().toISOString().split("T")[0]
+    datePicker.setAttribute("min", today)
 
-timerTitle = forms[1].querySelector(".timer-name")
-timerElements = forms[1].querySelectorAll("span")
-firstDistance = 0
-sec = 1000
-minute = sec * 60
-hour = minute * 60
-day = hour * 24
 
-function updateDom() {
-    timerActive = setInterval(() => {
-        if (!isPaused) {
-            const now = new Date().getTime()
-            const distance = inputValue - now
-            // resetTimer +=now
-            console.log(distance)
-            const days = Math.floor(distance / day)
-            const hours = Math.floor((distance % day) / hour)
-            const minutes = Math.floor((distance % hour) / minute)
-            const seconds = Math.floor((distance % minute) / sec)
-            console.log(days, hours, minutes, seconds)
-            if (days === 0 && hours === 0 && minutes === 0 && seconds === 0){
-                end()
+    let timerActive;
+
+
+    sec = 1000
+    minute = sec * 60
+    hour = minute * 60
+    day = hour * 24
+
+    function updateDom(f0rm, timerElements, inputValue) {
+        timerActive = setInterval(() => {
+            console.log(f0rm.nextElementSibling.className)
+            if (f0rm.nextElementSibling.className === "timer reset") {
+                f0rm.nextElementSibling.className = "timer play"
+                clearInterval(timerActive)
+            } else if (f0rm.nextElementSibling.className === "timer play") {
+                const now = new Date().getTime()
+                const distance = inputValue - now
+                // resetTimer +=now
+                console.log(distance)
+                const days = Math.floor(distance / day)
+                const hours = Math.floor((distance % day) / hour)
+                const minutes = Math.floor((distance % hour) / minute)
+                const seconds = Math.floor((distance % minute) / sec)
+                console.log(days, hours, minutes, seconds)
+                if (distance < 0) {
+                    end()
+                }
+                timerElements[0].textContent = `${days}`
+                timerElements[1].textContent = `${hours}`
+                timerElements[2].textContent = `${minutes}`
+                timerElements[3].textContent = `${seconds}`
+                f0rm.hidden = true
+                f0rm.nextElementSibling.hidden = false
             }
-            timerElements[0].textContent = `${days}`
-            timerElements[1].textContent = `${hours}`
-            timerElements[2].textContent = `${minutes}`
-            timerElements[3].textContent = `${seconds}`
-            forms[0].hidden = true
-            forms[1].hidden = false
+        }, sec)
+    }
+
+    function updateCountdown(e) {
+        e.preventDefault()
+        let timerTitle = this.nextElementSibling.querySelector(".timer-name")
+        let timerElements = this.nextElementSibling.querySelectorAll("span")
+        let inputTitle = e.target[0].value
+        let inputDate = e.target[1].value
+        if (inputDate === '') {
+            alert(`Please select a date for the Timer\nTimer name : "${inputTitle}"`)
+        } else {
+            let inputValue = new Date(inputDate).getTime()
+            timerTitle.textContent = `${inputTitle}`
+            isPaused = false
+            updateDom(this, timerElements, inputValue)
         }
-    }, sec)
-}
 
-function updateCountdown(e) {
-    e.preventDefault()
-    inputTitle = e.srcElement[0].value
-    inputDate = e.srcElement[1].value
-    console.log(inputTitle, inputDate)
-    if (inputDate === '') {
-        alert(`Please select a date for the Timer\nTimer name : "${inputTitle}"`)
-    } else {
-        inputValue = new Date(inputDate).getTime()
-        timerTitle.textContent = `${inputTitle}`
-        updateDom()
     }
 
-}
+    function reset(e) {
+        f1rm = this.closest(".timer")
+        f1rm.querySelector("img").setAttribute("src", "./img/svg/pause.svg")
+        e.preventDefault()
+        f1rm.previousElementSibling.hidden = false
+        f1rm.hidden = true
 
-function reset(e) {
-    e.preventDefault()
-    forms[0].hidden = false
-    forms[1].hidden = true
-
-    clearInterval(timerActive)
-
-    inputTitle = ""
-    inputDate = ""
-
-}
-
-function end(e) {
-    e.preventDefault()
-    forms[0].hidden = true
-    forms[1].hidden = true
-    forms[2].hidden = false
-    clearInterval(timerActive)
-    inputTitle = ""
-    inputDate = ""
+        f1rm.className = "timer reset"
 
 
-}
-
-forms[0].addEventListener("submit", updateCountdown)
-forms[1].querySelectorAll(".timer-button")[1].addEventListener('click', reset)
-forms[1].querySelectorAll(".timer-button")[2].addEventListener('click', end)
-forms[1].querySelectorAll(".timer-button")[0].addEventListener('click', () => {
-    if (isPaused) {
-        isPaused = false
-        forms[1].querySelector("img").setAttribute("src", "./img/svg/pause.svg")
-    } else {
-        isPaused = true
-        forms[1].querySelector("img").setAttribute("src", "./img/svg/play.svg")
     }
-})
+
+    function end(e) {
+        e.preventDefault()
+        f1rm = this.closest(".timer")
+        f1rm.previousElementSibling.hidden = true
+        f1rm.hidden = true
+        f1rm.nextElementSibling.hidden = false
+        f1rm.className = "timer reset"
+
+
+    }
+
+    function pause() {
+        f1rm = this.closest(".timer")
+        console.log(f1rm)
+        if (f1rm.classList[1] === "play") {
+            f1rm.classList.remove("play")
+            f1rm.querySelector("img").setAttribute("src", "./img/svg/play.svg")
+        } else {
+            f1rm.classList.add("play")
+            f1rm.querySelector("img").setAttribute("src", "./img/svg/pause.svg")
+        }
+    }
+
+    function die(){
+        this.closest(".timer-container").remove()
+    }
+    forms[0].addEventListener("submit", updateCountdown)
+    forms[0].querySelectorAll(".timer-button")[1].addEventListener("click", die)
+    forms[1].querySelectorAll(".timer-button")[1].addEventListener('click', reset)
+    forms[1].querySelectorAll(".timer-button")[2].addEventListener('click', end)
+    forms[1].querySelector(".timer-button").addEventListener('click', pause)
+    forms[2].querySelector(".timer-button").addEventListener('click', die)
+
+}
+
+const template = document.querySelector("#newTimer");
+
+function createTime() {
+    const clone = template.content.cloneNode(true);
+    adder.parentNode.insertBefore(clone, adder)
+    timeMe()
+}
+
+adder = document.querySelector("#add-bottom")
+adder.addEventListener('click', createTime)
+
