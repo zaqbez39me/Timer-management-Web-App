ad67413b9f58ea862ba8267c9876517ce2e38a19
VadigullinElmir1 <89780734+VadigullinElmir1@users.noreply.github.com>
2023-05-19 11:11:08 +0300

    Update script_3.js

--- web_app/script_3.js
+++ web_app/script_3.js
@@ -1,28 +1,31 @@
-function timeMe() {
+sec = 1000
+minute = sec * 60
+hour = minute * 60
+day = hour * 24
+
+
+function timeMe() { // Функция добавляет таймеру слушателей событий для кнопок
+
+
     containers = document.getElementsByClassName("timer-container")
     forms = containers[containers.length - 1].getElementsByClassName("timer")
     datePicker = forms[0].querySelectorAll(".input-date")[1]
     const today = new Date().toISOString().split("T")[0]
     datePicker.setAttribute("min", today)
 
-
+    // Интервал
     let timerActive;
 
-
-    sec = 1000
-    minute = sec * 60
-    hour = minute * 60
-    day = hour * 24
-
     function updateDom(f0rm, timerElements, inputValue) {
+        let now = new Date().getTime()
         timerActive = setInterval(() => {
             console.log(f0rm.nextElementSibling.className)
             if (f0rm.nextElementSibling.className === "timer reset") {
                 f0rm.nextElementSibling.className = "timer play"
                 clearInterval(timerActive)
             } else if (f0rm.nextElementSibling.className === "timer play") {
-                const now = new Date().getTime()
                 const distance = inputValue - now
+                now += sec
                 // resetTimer +=now
                 console.log(distance)
                 const days = Math.floor(distance / day)
@@ -44,18 +47,19 @@ function timeMe() {
     }
 
     function updateCountdown(e) {
-        e.preventDefault()
-        let timerTitle = this.nextElementSibling.querySelector(".timer-name")
-        let timerElements = this.nextElementSibling.querySelectorAll("span")
-        let inputTitle = e.target[0].value
-        let inputDate = e.target[1].value
+        f0rm = this.closest(".timer")
+        let timerTitle = f0rm.nextElementSibling.querySelector(".timer-name")
+        let timerElements = f0rm.nextElementSibling.querySelectorAll("span")
+        inputTitle = f0rm.querySelector(".input_name").value
+        let inputDate = f0rm.querySelectorAll(".input-date")[1].value
+        console.log(inputDate)
         if (inputDate === '') {
             alert(`Please select a date for the Timer\nTimer name : "${inputTitle}"`)
         } else {
             let inputValue = new Date(inputDate).getTime()
             timerTitle.textContent = `${inputTitle}`
             isPaused = false
-            updateDom(this, timerElements, inputValue)
+            updateDom(f0rm, timerElements, inputValue)
         }
 
     }
@@ -63,7 +67,6 @@ function timeMe() {
     function reset(e) {
         f1rm = this.closest(".timer")
         f1rm.querySelector("img").setAttribute("src", "./img/svg/pause.svg")
-        e.preventDefault()
         f1rm.previousElementSibling.hidden = false
         f1rm.hidden = true
 
@@ -73,7 +76,6 @@ function timeMe() {
     }
 
     function end(e) {
-        e.preventDefault()
         f1rm = this.closest(".timer")
         f1rm.previousElementSibling.hidden = true
         f1rm.hidden = true
@@ -95,31 +97,39 @@ function timeMe() {
         }
     }
 
-    function die(){
+    function die() {
         this.closest(".timer-container").remove()
     }
-    forms[0].addEventListener("submit", updateCountdown)
+
+    forms[0].querySelector(".timer-button").addEventListener("click", updateCountdown)
     forms[0].querySelectorAll(".timer-button")[1].addEventListener("click", die)
+
+    forms[1].querySelector(".timer-button").addEventListener('click', pause)
     forms[1].querySelectorAll(".timer-button")[1].addEventListener('click', reset)
     forms[1].querySelectorAll(".timer-button")[2].addEventListener('click', end)
-    forms[1].querySelector(".timer-button").addEventListener('click', pause)
+
     forms[2].querySelector(".timer-button").addEventListener('click', die)
 
 }
 
-const template = document.querySelector("#newTimer");
+
 
 function createTime() {
     const clone = template.content.cloneNode(true);
     adder.parentNode.insertBefore(clone, adder)
+    // Новый таймер был создан
+
     timeMe()
 }
 
+// Шаблон таймера
+const template = document.querySelector("#newTimer");
+
+// Кнопки добавления таймеров
 madder = document.querySelector(".header__add")
 adder = document.querySelector("#add-bottom")
 adder.addEventListener('click', createTime)
 madder.addEventListener('click', createTime)
-
 
 let exitBtn = document.querySelector(".header__exit")
 
